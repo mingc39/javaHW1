f2d381d67a32befc1682f12523f6295827044bf0
mingc39 <43945033+mingc39@users.noreply.github.com>
2018-11-21 10:33:01 +0900

    x버튼 눌렀을때 진짜 닫을지 묻고 닫기

--- Project1/src/UIMain.java
+++ Project1/src/UIMain.java
@@ -2,6 +2,8 @@ import java.awt.Event;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -54,7 +56,13 @@ public class UIMain extends JFrame {
 		add(scroll);
 		
 		// 창 기본 설정
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				if(JOptionPane.showConfirmDialog(null, "정말로 종료하시겠습니까?", "종료", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE) == JOptionPane.YES_OPTION)
+					System.exit(0);
+			}
+		});
 		setSize(1000,500);
 		setVisible(true);
 		
