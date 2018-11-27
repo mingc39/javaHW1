9aeef152a830c4f5de180c73fd974af6db437051
mingc39 <43945033+mingc39@users.noreply.github.com>
2018-11-27 10:31:07 +0900

    사용하지 않는 패키지에 대한 import 제거

--- Project1/src/UISearch.java
+++ Project1/src/UISearch.java
@@ -1,16 +1,11 @@
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
