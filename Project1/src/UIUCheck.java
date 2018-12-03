cde6d3fa24a932581079e086727e2b9195038f48
Dayeon1351 <zzzz1351@gmail.com>
2018-12-03 11:42:27 +0900

    쓸모없는 생성자 삭제 및 창 이름 변경

--- Project1/src/UIUCheck.java
+++ Project1/src/UIUCheck.java
@@ -27,135 +27,10 @@ public class UIUCheck extends JFrame {
 	int nextint = 0;
 	int nextint2 = 0;
 
-	// 쓸모가 없는 생성자
-	public UIUCheck(StudentTable stu) {
-		setTitle("나는 수정-입력의 출석입니다.");
-
-		box = Box.createVerticalBox();
-		save = new JButton("저장");
-
-		for (int i = 1; i < 17; i++) {
-			p = new JPanel();
-			tue = new JLabel("화요일");
-			thr = new JLabel("목요일");
-
-			attend = new JRadioButton("출석");
-			absent = new JRadioButton("결석               ");
-			late = new JRadioButton("지각");
-
-			check = new ButtonGroup();
-			check.add(attend);
-			check.add(absent);
-			check.add(late);
-
-			ActionListener al = new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					String s = e.getActionCommand();
-					if (s.equals(attend.getText())) {
-						tf = 0;
-					} else if (s.equals(late.getText())) {
-						tf = 1;
-					} else if (s.equals(absent.getText())) {
-						tf = 2;
-					}
-					a[nextint++][0] = tf;
-
-				}
-			};
-			attend.addActionListener(al);
-			absent.addActionListener(al);
-			late.addActionListener(al);
-
-			w = i + "주               ";
-			week = new JLabel(w);
-
-			days = Box.createHorizontalBox();
-			days.add(tue);
-
-			ver = Box.createVerticalBox();
-			ver.add(days);
-
-			info = Box.createHorizontalBox();
-			info.add(attend);
-			info.add(late);
-			info.add(absent);
-			ver.add(info);
-
-			p.add(week);
-			p.add(ver);
-
-			days = Box.createHorizontalBox();
-			days.add(thr);
-
-			attend = new JRadioButton("출석");
-			late = new JRadioButton("지각");
-			absent = new JRadioButton("결석               ");
-
-			check = new ButtonGroup();
-			check.add(attend);
-			check.add(absent);
-			check.add(late);
-
-			ActionListener aal = new ActionListener() {
-
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					String s = e.getActionCommand();
-					if (s.equals(attend.getText())) {
-						tf = 0;
-					} else if (s.equals(late.getText())) {
-						tf = 1;
-					} else if (s.equals(absent.getText())) {
-						tf = 2;
-					}
-					a[nextint2++][1] = tf;
-				}
-			};
-			attend.addActionListener(aal);
-			absent.addActionListener(aal);
-			late.addActionListener(aal);
-
-			ver = Box.createVerticalBox();
-			ver.add(days);
-			info = Box.createHorizontalBox();
-			info.add(attend);
-			info.add(late);
-			info.add(absent);
-			ver.add(info);
-
-			p.add(ver);
-			box.add(p);
-		}
-
-		ActionListener sal = new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				for (int i = 0; i < 16; i++) {
-					for (int j = 0; j < 2; j++) {
-						System.out.println(a[i][j]);
-					}
-				}
-				
-				
-			}
-		};
-
-		save.addActionListener(sal);
-		this.add(box, BorderLayout.CENTER);
-		p = new JPanel();
-		p.add(save, BorderLayout.CENTER);
-		this.add(p, BorderLayout.SOUTH);
-
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		this.pack();
-		setVisible(true);
-	}
 
 	// 쓸모가 있는 생성자
 	public UIUCheck(int[][] attendance) {
-		setTitle("나는 수정-수정의 출석입니다.");
+		setTitle("출석체크");
 
 		box = Box.createVerticalBox();
 		save = new JButton("저장");
@@ -305,6 +180,7 @@ public class UIUCheck extends JFrame {
 				for(int i = 0; i < 16; i++) attendance[i] = a[i]; 
 				//stu.editStudent(student, index);
 				//System.out.println(stu.getAttendanceScore());
+				
 			}
 		};
 
